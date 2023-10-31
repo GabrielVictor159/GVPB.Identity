cfda4999da5b854f8b0b6cff76112bc47b535a08
Gabriel <gabrielvictor159487@gmail.com>
2023-10-31 20:30:51 -0300

    fix opcional comunicationsHandlers

--- src/GVPB.Identity.Application/UseCases/Handler.cs
+++ src/GVPB.Identity.Application/UseCases/Handler.cs
@@ -3,12 +3,12 @@ namespace GVPB.Identity.Application.UseCases;
 
 public abstract class Handler<T, F>
 {
-    protected Handler<T, F>? sucessor;
-    public dynamic SetSucessor(Handler<T, F> sucessor)
+    protected Handler<T, F?>? sucessor;
+    public dynamic SetSucessor(Handler<T, F?> sucessor)
     {
         this.sucessor = sucessor;
         return this;
     }
-    public abstract void ProcessRequest(T request, F comunications);
+    public abstract void ProcessRequest(T request, F? comunications);
 }
 
