45eaf3deca9330ceb09b6027a9770d764b05c3ae
Gabriel <gabrielvictor159487@gmail.com>
2024-03-25 16:52:36 -0300

    fix bugs

--- src/GVPB.Identity.Application/UseCases/RequestUser/Handlers/VerifyDisponibilityTermsHandler.cs
+++ src/GVPB.Identity.Application/UseCases/RequestUser/Handlers/VerifyDisponibilityTermsHandler.cs
@@ -3,6 +3,7 @@ using GVPB.Identity.Application.Interfaces.Services;
 using GVPB.Identity.Application.UseCases;
 using GVPB.Identity.Domain.Enum;
 using GVPB.Identity.Domain.Models;
+using Newtonsoft.Json;
 
 namespace GVPB.Identity.Application.UseCases.RequestUser.Handlers;
 
@@ -10,25 +11,36 @@ public class VerifyDisponibilityTermsHandler : Handler<RequestUserRequest, Reque
 {
     private readonly INotificationService notificationService;
     private readonly IUserRepository userRepository;
+    private readonly IRequestUserRepository requestUserRepository;
 
-    public VerifyDisponibilityTermsHandler(INotificationService notificationService, IUserRepository userRepository)
+    public VerifyDisponibilityTermsHandler
+        (INotificationService notificationService, 
+        IUserRepository userRepository, 
+        IRequestUserRepository requestUserRepository)
     {
         this.notificationService = notificationService;
         this.userRepository = userRepository;
+        this.requestUserRepository = requestUserRepository;
     }
 
     protected override void ProcessRequest(RequestUserRequest request, RequestUserComunications? comunications)
     {
         VerifyProperty(request.NewUser, u => u.UserName, "existing username", "user name", request);
         VerifyProperty(request.NewUser, u => u.Email, "existing email", "email", request);
-        
-        if(notificationService.HasNotifications)
+        VerifyRequestUserEmail(request.NewUser.Email, "existing email request", request);
+
+        if (notificationService.HasNotifications)
         {
             Break();
             return;
         }
     }
-
+    private void VerifyRequestUserEmail(string email, string notificationKey, RequestUserRequest request)
+    {
+        var existingUser = requestUserRepository.GetByFilter(e => JsonConvert.DeserializeObject<User>(e.Body)!.Email == email).ToList().Count;
+        if (existingUser >= 3)
+            notificationService.AddNotification(notificationKey, $"{request.Localizer.GetKey("LOGINNOTFOUND").Value} Email={email}");
+    }
     private void VerifyProperty(User user, Func<User, string> propertySelector, string notificationKey, string propertyDescription, RequestUserRequest request)
     {
         var value = propertySelector(user);
